327072afd87961097a832519a57705116592b5e6
Digidaaw <unknown151812@gmail.com>
2026-06-07 06:37:26 +0700

    Reapply "Merge pull request #20 from Digidaaw/maulana-branch"
    This reverts commit ee52e7f595f132a26859e95f99fe21612676fa0c.

---
+++ tests/Unit/layananTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class layananTest extends TestCase
+{
+    public function test_layanan_test_file_is_available(): void
+    {
+        $this->assertTrue(true);
+    }
+}
